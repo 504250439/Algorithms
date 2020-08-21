ab7f93d975be7f38c0f7dcc47a97bf21286ef8f8
qiuwenhao <504250439@qq.com>
2020-08-21 17:07:58 +0800

    8.21

--- 剑指offer/code/src/t11.java
+++ 剑指offer/code/src/t11.java
@@ -1,15 +1,38 @@
 public class t11 {
+
+
 }
+
+//把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
+
 
 //线性查找法
-class Solution {
-    public int minArray(int[] numbers) {
-        for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i]<numbers[i-1]){
-                return numbers[i];
-            }
-        }
-        return numbers[0];
+//class Solution {
+//    public int minArray(int[] numbers) {
+//        for (int i = 1; i < numbers.length; i++) {
+//            if (numbers[i]<numbers[i-1]){
+//                return numbers[i];
+//            }
+//        }
+//        return numbers[0];
+//
+//    }
+//}
 
-    }
-}
+
+//使用二分法,
+//主要问题在当   m==j 的时候,不知道最小的数应该再左边定系右边,这时候我们就需要将j向前移动一个位置即可.
+
+//class Solution {
+//    public int minArray(int[] numbers) {
+//        int i = 0, j = numbers.length - 1;
+//        while (i < j) {
+//            int m = (i + j) / 2;
+//            if (numbers[m] > numbers[j]) i = m + 1;
+//            else if (numbers[m] < numbers[j]) j = m;
+//            else j--;
+//        }
+//        return numbers[i];
+//    }
+//}
+//
---
+++ 剑指offer/code/src/t13.java
@@ -0,0 +1,88 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class t13 {
+}
+
+
+
+//与上一题相近,可以利用深度优先算法和广度优先算法来解决
+
+// class Solution {
+//     public int movingCount(int m, int n, int k) {
+//         //临时变量visited记录格子是否被访问过
+//         boolean[][] visited = new boolean[m][n];
+//         return dfs(0, 0, m, n, k, visited);
+//     }
+//
+//     public int dfs(int i, int j, int m, int n, int k, boolean[][] visited) {
+//         //i >= m || j >= n是边界条件的判断，k < sum(i, j)判断当前格子坐标是否
+//         // 满足条件，visited[i][j]判断这个格子是否被访问过
+//         if (i >= m || j >= n || k < sum(i, j) || visited[i][j])
+//             return 0;
+//         //标注这个格子被访问过
+//         visited[i][j] = true;
+//         //沿着当前格子的右边和下边继续访问
+//         return 1 + dfs(i + 1, j, m, n, k, visited) + dfs(i, j + 1, m, n, k, visited);
+//     }
+//
+//     //计算两个坐标数字的和
+//     private int sum(int i, int j) {
+//         int sum = 0;
+//         while (i != 0) {
+//             sum += i % 10;
+//             i /= 10;
+//         }
+//         while (j != 0) {
+//             sum += j % 10;
+//             j /= 10;
+//         }
+//         return sum;
+//     }
+//
+//}
+
+//
+//     public int movingCount(int m, int n, int k) {
+//         //临时变量visited记录格子是否被访问过
+//         boolean[][] visited = new boolean[m][n];
+//         int res = 0;
+//         //创建一个队列，保存的是访问到的格子坐标，是个二维数组
+//         Queue<int[]> queue = new LinkedList<>();
+//         //从左上角坐标[0,0]点开始访问，add方法表示把坐标
+//         // 点加入到队列的队尾
+//         queue.add(new int[]{0, 0});
+//         while (queue.size() > 0) {
+//             //这里的poll()函数表示的是移除队列头部元素，因为队列
+//             // 是先进先出，从尾部添加，从头部移除
+//             int[] x = queue.poll();
+//             int i = x[0], j = x[1];
+//             //i >= m || j >= n是边界条件的判断，k < sum(i, j)判断当前格子坐标是否
+//             // 满足条件，visited[i][j]判断这个格子是否被访问过
+//             if (i >= m || j >= n || k < sum(i, j) || visited[i][j])
+//                 continue;
+//             //标注这个格子被访问过
+//             visited[i][j] = true;
+//             res++;
+//             //把当前格子右边格子的坐标加入到队列中
+//             queue.add(new int[]{i + 1, j});
+//             //把当前格子下边格子的坐标加入到队列中
+//             queue.add(new int[]{i, j + 1});
+//         }
+//         return res;
+//     }
+//
+//     //计算两个坐标数字的和
+//     private int sum(int i, int j) {
+//         int sum = 0;
+//         while (i != 0) {
+//             sum += i % 10;
+//             i /= 10;
+//         }
+//         while (j != 0) {
+//             sum += j % 10;
+//             j /= 10;
+//         }
+//         return sum;
+//     }
+//
