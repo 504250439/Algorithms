c026cefe4413728e5de6dcfe27f91d5a06833db3
qiuwenhao <504250439@qq.com>
2020-08-31 10:57:58 +0800

    8.31.二叉树的层次遍历,又是天气美好的一天

---
+++ 剑指offer/code/src/t31.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class t31 {
+}
+
+
+//这道题笔试经常考,我们只需要利用一个辅助栈去模拟push和pop,当他们栈顶相同的时候进行操作
+//最后直接判断能否将栈pop到为空即可
+//class Solution {
+//    public boolean validateStackSequences(int[] pushed, int[] popped) {
+//        Stack<Integer> stack=new Stack<>();
+//        int j=0;
+//        for (int i = 0; i < pushed.length; i++) {
+//            stack.push(pushed[i]);
+//            while (!stack.isEmpty() &&popped[j]==stack.peek()){
+//                stack.pop();
+//                j++;
+//            }
+//        }
+//        return stack.isEmpty();
+//    }
+//}
+//
+//
+//
---
+++ 剑指offer/code/src/t32.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class t32 {
+}
+
+
+
+//class TreeNode {
+//      int val;
+//      TreeNode left;
+//      TreeNode right;
+//      TreeNode(int x) { val = x; }
+//  }
+//class Solution {
+//    public int[] levelOrder(TreeNode root) {
+//        if (root == null) {
+//            return new int[0];
+//        }
+//        Queue<TreeNode> queue=new LinkedList<>();
+//        queue.add(root);
+//        ArrayList<Integer> list=new ArrayList<>();
+//        while (!queue.isEmpty()){
+//            root=queue.poll();
+//            if (root.left != null) {
+//                queue.add(root.left);
+//            }
+//            if (root.right != null) {
+//                queue.add(root.right);
+//            }
+//            int a=root.val;
+//            list.add(a);
+//        }
+//        int[] res=new int[list.size()];
+//        int i=0;
+//        for(Integer num:list){
+//            res[i]=num;
+//            i++;
+//        }
+//        return res;
+//
+//    }
+//}
