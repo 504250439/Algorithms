4ebca13033bdaae321c51ba68bd54e0e7a978c0f
qiuwenhao <504250439@qq.com>
2020-09-03 18:04:32 +0800

    9.3 更新一下38题

--- 剑指offer/code/src/offer/t38.java
+++ 剑指offer/code/src/offer/t38.java
@@ -1,6 +1,7 @@
 package offer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -8,55 +9,70 @@ public class t38 {
 }
 
 
-//class Exam38_permutation {
-//    List<String> list = new ArrayList<>();
-//    //为了让递归函数添加结果方便，定义到函数之外，这样无需带到递归函数的参数列表中
-//    char[] c;
-//    //同；但是其赋值依赖c，定义声明分开
+//class Solution {
 //    public String[] permutation(String s) {
-//        c = s.toCharArray();
-//        //从第一层开始递归
-//        dfs(0);
-//        return list.toArray(new String[list.size()]);
-//        //将字符串数组ArrayList转化为String类型数组
+//        int len = s.length();
+//        if (len == 0) {
+//            return new String[0];
+//        }
+//
+//        // 转换成字符数组是常见的做法
+//        char[] charArr = s.toCharArray();
+//        // 排序是为了去重方便
+//        Arrays.sort(charArr);
+//
+//        // 由于操作的都是字符，使用 StringBuilder
+//        StringBuilder path = new StringBuilder();
+//        boolean[] used = new boolean[len];
+//
+//        // 为了方便收集结果，使用动态数组
+//        List<String> res = new ArrayList<>();
+//        dfs(charArr, len, 0, used, path, res);
+//
+//        // 记得转成字符串数组
+//        return res.toArray(new String[0]);
 //    }
 //
-//    private void dfs(int x) {
-//        //当递归函数到达第三层，就返回，因为此时第二第三个位置已经发生了交换
-//        if (x == c.length - 1) {
-//            list.add(String.valueOf(c));//将字符数组转换为字符串
+//    /**
+//     * @param charArr 字符数组
+//     * @param len     字符数组的长度
+//     * @param depth   当前递归深度
+//     * @param used    当前字符是否使用
+//     * @param path    从根结点到叶子结点的路径
+//     * @param res     保存结果集的变量
+//     */
+//    private void dfs(char[] charArr,
+//                     int len,
+//                     int depth,
+//                     boolean[] used,
+//                     StringBuilder path,
+//                     List<String> res) {
+//        if (depth == len) {
+//            // path.toString() 恰好生成了新的字符对象
+//            res.add(path.toString());
 //            return;
 //        }
-//        //为了防止同一层递归出现重复元素
-//        HashSet<Character> set = new HashSet<>();
-//        //这里就很巧妙了,第一层可以是a,b,c那么就有三种情况，这里i = x,正巧dfs(0)，正好i = 0开始
-//        // 当第二层只有两种情况，dfs(1）i = 1开始
-//        for (int i = x; i < c.length; i++){
-//            //发生剪枝，当包含这个元素的时候，直接跳过
-//            if (set.contains(c[i])){
-//                continue;
+//        for (int i = 0; i < len; i++) {
+//            if (!used[i]) {
+//                if (i > 0 && charArr[i] == charArr[i - 1] && !used[i - 1]) {
+//                    continue;
+//                }
+//                used[i] = true;
+//                path.append(charArr[i]);
+//
+//                dfs(charArr, len, depth + 1, used, path, res);
+//
+//                // 递归完成以后，需要撤销选择，递归方法执行之前做了什么，递归方法执行以后就需要做相应的逆向操作
+//                path.deleteCharAt(path.length() - 1);
+//                used[i] = false;
 //            }
-//            set.add(c[i]);
-//            //交换元素，这里很是巧妙，当在第二层dfs(1),x = 1,那么i = 1或者 2， 要不是交换1和1，要不交换1和2
-//            swap(i,x);
-//            //进入下一层递归
-//            dfs(x + 1);
-//            //返回时交换回来，这样保证到达第1层的时候，一直都是abc。这里捋顺一下，开始一直都是abc，那么第一位置总共就3个位置
-//            //分别是a与a交换，这个就相当于 x = 0, i = 0;
-//            //     a与b交换            x = 0, i = 1;
-//            //     a与c交换            x = 0, i = 2;
-//            //就相当于上图中开始的三条路径
-//            //第一个元素固定后，每个引出两条路径,
-//            //     b与b交换            x = 1, i = 1;
-//            //     b与c交换            x = 1, i = 2;
-//            //所以，结合上图，在每条路径上标注上i的值，就会非常容易好理解了
-//            swap(i,x);
 //        }
 //    }
 //
-//    private void swap(int i, int x) {
-//        char temp = c[i];
-//        c[i] = c[x];
-//        c[x] = temp;
+//    public static void main(String[] args) {
+//        Solution solution = new Solution();
+//        String[] res = solution.permutation("aba");
+//        System.out.println(Arrays.toString(res));
 //    }
+//
 //}
